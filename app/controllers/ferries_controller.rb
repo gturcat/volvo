3880edf487921c0583e502340a3bea6de000de5b
guillaume turcat <gturcat@me.com>
2019-04-11 22:15:28 +0200

    formulaire de convoyage a finir

--- app/controllers/ferries_controller.rb
+++ app/controllers/ferries_controller.rb
@@ -3,7 +3,31 @@ class FerriesController < ApplicationController
     @ferries = Ferry.all
   end
 
+
+  def new
+  @ferry = Ferry.new
+  @bus = Bus.find(params[:bus_id])
+  end
+
+  def create
+    @ferry = Ferry.new(ferry_params)
+    @bus = Bus.find(params[:bus_id])
+    @ferry.bus = @bus
+    if @ferry.save
+      redirect_to bus_path(@ferry.bus[:id])
+    else
+      render :new
+    end
+  end
+
   def show
     @ferry = Ferry.find(params[:id])
   end
+
+  private
+
+  def ferry_params
+    params.require(:ferry).permit(:depart, :arrivee, :date_convoyage, :site, :note, :numero_bdc)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'ferries/index'
-  get 'ferries/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :buses
+  resources :ferries, only: [:index, :show]
+  resources :buses do
+    resources :ferries, only: [:new, :create]
+  end
   resources :orders
   resources :factory_orders, only: [:show, :edit, :update]
   resources :documents, only: [:new, :create, :show, :edit, :update]
